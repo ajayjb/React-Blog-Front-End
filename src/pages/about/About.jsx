21f1f48faf66044983f22e5b65906f3743dccf36
Ajay <ajayjb11@gmail.com>
2021-11-28 14:02:03 +0530

    Contact page added

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./about.css";
+
+export default function About() {
+    return (
+        <div className="about">
+           <img className="aboutImg" src="https://i.imgur.com/X0kjFoX.jpg" alt="Web Development"/>
+           <p className="aboutPara">
+            Hi this is Ajay, welcome to my Moderen Blog website. Wait Wait I know this not a great about page, clam down I'm just learning, first let me give introduction about my boring life. Here we go. I'm a software developer based in Bengaluru. Basically I'm from mechanical background, worked as Mechanical Design 
+            Engineer for 2.5 years. Every day as a Mechanical Engineer was like hell, Later I decided to quit my job beacause during that period of time I never felt so excited or passionate about what I'm doing. 
+            During my college days I always wanted to learn software coding, due to lack of guidance, I couldn't able to follow my dream. So this time I'm
+            not going to make the same mistake. Few months back I started over. started learning coding on my own. When I'm coding its just feels like this is the thing I really wanted do in the whole world. currently I'm learning Web Development, I made this Moderen Blog website with what I have learn't till today. 
+            I made this site using ReactJS, NodeJS and MongoDB. In this Moderen Blog website users can  write your own blogs on technical 
+            field, so that other people can read about them. My final goal is to learn Blockchain Technology and get into Devops. So finally this is about me and my
+            Moderen blog website. Good bye, have a great life.
+           </p>
+        </div>
+    )
+}
---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import "./contact.css"
+
+export default function Contact() {
+    const [sent, setSent] = useState(false);
+    const [name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const [text, setText] = useState("");
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const res  = await axios.post("http://localhost:5000/api/contacts/", {
+            name : name,
+            email : email,
+            message : text
+        });
+        setSent(true);
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+    }
+    return (
+        <div className="contact">
+            <span className="heading0">GOT A QUESTION?</span>
+            <h3 className="heading1">Contact Moderen Blog</h3>
+            <p className="para1">We’re here to help and answer any question you might have. We look forward to hearing from you 🙂</p>
+            <form className="contactForm" onSubmit={handleSubmit}>
+              <label className="name1" htmlFor="nameInput">Name</label>
+              <input className="nameInput" type="text" onChange={(e)=>{ setName(e.target.value) }} placeholder="Name"/>
+              <label className="email" htmlFor="emailInput">Email</label>
+              <input className="emailInput" type="email" onChange={(e)=>{ setEmail(e.target.value) }} placeholder="Email"/>
+              <label className="message">Message</label>
+              <textarea className="messageInput" onChange={(e)=>{ setText(e.target.value) }} cols="30" rows="8"></textarea>
+              <button className="contactSubmit" type="submit">Send Message</button>
+            </form>
+            {sent? <span className="sent">"Message sent successfully"</span>: <span></span>}
+        </div>
+    )
+}
